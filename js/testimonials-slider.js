f2b521ec7875cf97855e0dbf99a7905fd5a342a3
kisirai <vlogatskiy3@gmail.com>
2024-09-06 04:16:17 +0300

    added slides

---
+++ js/testimonials-slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".testimonials-slides").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    dots: true,
+    draggable: false
+  })
+})
