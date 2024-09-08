862c228964f53efe9512123212c106c8f106c1cb
kisirai <vlogatskiy3@gmail.com>
2024-09-09 02:40:39 +0300

    added alert in order of wrong input

--- js/app.js
+++ js/app.js
@@ -29,33 +29,23 @@ const formPhone = document.getElementById('form-phone');
 const formEmail = document.getElementById('form-email');
 
 formSumbit.addEventListener("click", function () {
-  let valid = true;
+  let nonValid = "";
   formName.addEventListener("input", function () { validityCheck(this, /^[a-zа-яіїґєґ]{5,}$/i) });
   formSurname.addEventListener("input", function () { validityCheck(this, /^[a-zа-яіїґєґ]{5,}$/i) });
   formInterested.addEventListener("input", function () { validityCheck(this, /(developer)|(qa)/i) });
   formPhone.addEventListener("input", function () { validityCheck(this) });
   formEmail.addEventListener("input", function () { validityCheck(this) });
 
-  valid &= validityCheck(formName, /[a-zа-яіїґєґ]{5,}/i);
-  valid &= validityCheck(formSurname, /[a-zа-яіїґєґ]{5,}/i);
-  valid &= validityCheck(formInterested, /(developer)|(qa)/i);
-  valid &= validityCheck(formPhone);
-  valid &= validityCheck(formEmail);
+  nonValid += validityCheck(formName, /[a-zа-яіїґєґ]{5,}/i);
+  nonValid += validityCheck(formSurname, /[a-zа-яіїґєґ]{5,}/i);
+  nonValid += validityCheck(formInterested, /(developer)|(qa)/i);
+  nonValid += validityCheck(formPhone);
+  nonValid += validityCheck(formEmail);
 
-  console.log("hie, it is before fetching");
-  
-  fetch("https://xhyeuwx.localto.net/hellow.html")
-    .then(response => response.text())
-    .then(html => {
-      console.log(html);
-    })
-    .catch(error => {
-      console.error(`Error fetching the HTML: ${error}`);
-    })
-  console.log("hie, it is after fetching!!!");
-  
-  if (!valid)
-    return;
+  if (nonValid != ""){
+    alert(`Виправіть будь ласка дані помилки: ${nonValid}`);
+    return;  
+  }
 
   formSumbit.style.backgroundColor = '#40a617';
   formSumbit.style.cursor = 'default';
@@ -69,18 +59,18 @@ function validityCheck(target, pattern = null) {
   if (pattern == null) {
     if (target.validity.valid) {
       target.classList.remove('form-input-wrong');
-      return true;
+      return "";
     }
     target.classList.add('form-input-wrong');
-    return false;
+    return `\n${target.placeholder}`;
   }
 
   if (target.value.match(pattern) != null) {
     target.classList.remove('form-input-wrong');
-    return true;
+    return "";
   }
   target.classList.add('form-input-wrong');
-  return false;
+  return `\n${target.placeholder}`;
 }
 
 
